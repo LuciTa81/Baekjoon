945164bcddd6037efb1eb549b83f3d9beed9a389
MinJoon <101696460+LuciTa81@users.noreply.github.com>
2022-04-06 19:41:39 +0900

    Update 두 수 비교하기.cpp

--- 단계별로 풀어보기/조건문/두 수 비교하기.cpp
+++ 단계별로 풀어보기/조건문/두 수 비교하기.cpp
@@ -1,1 +1,12 @@
-d
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	if (a > b) { cout << '>'; }
+	else if (a < b) { cout << '<'; } //큰 따옴표는 문자열 받을수 있고, 작은 따옴표는 char로 한글자 받고, 그걸 아스키코드로 변경된후 처리된다.
+	else { cout << "=="; }
+}
